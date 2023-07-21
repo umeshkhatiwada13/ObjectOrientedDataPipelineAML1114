56b6fedfa35a269f7c2b794bab00c318fecb86f8
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2023-07-21 17:42:51 -0400

    fix: complete pivot function

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # This is a sample Python script.
 
+# Student name and roll number
+
 from pre_processing import DataReader
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("I am running")
-    # DataReader.read_file()
     dataReader = DataReader.DataReader
     dataReader.read_file()
     dataReader.pivot_columns()
